c0af3f3c229688b4e5f47e18aa0abbd6bcbcb1c1
Keith C. Ivey <keith@iveys.org>
2018-08-31 22:12:42 -0400

    work on adding ratings to get-candidates.js

--- get-candidates.js
+++ get-candidates.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const cheerio = require('cheerio');
+const yaml = require('js-yaml');
 const request = require('./request');
 const url = 'https://en.wikipedia.org/wiki/United_States_House_of_Representatives_elections,_2018';
 const normalizeState = require('us-states-normalize');
@@ -9,28 +10,35 @@ request(url).then(processHtml);
 
 function processHtml(html) {
     const $ = cheerio.load(html.replace(/<br\s*\/?>/g, '\n'));
+    let data = [];
     $('table.wikitable').each(
         (i, table) => {
             let $table = $(table);
             let text1 = $table.find('tr').eq(0).find('th').eq(0).text().trim();
             let text2 = $table.find('tr').eq(1).find('th').eq(0).text().trim();
-            if (text1 === 'District' && text2 === 'Location') {
-                let tableData = processTable($, $table);
+            if (text1 === 'District') {
+                if (text2 === 'Location') {
+                    data.push(...processTable($, $table, 2));
+                }
+                else {
+                    console.log(processTable($, $table, 1));
+                    process.exit();
+                }
             }
         }
     );
+    console.log(yaml.safeDump(data));
 }
 
-function processTable($, $table) {
+function processTable($, $table, headerRows) {
     let heads = [];
     let tableData = [];
-    let skipRow = false;
     $table.find('tr').each(
         (rowIndex, row) => {
             let $row = $(row);
             let rowData = {};
             let columnIndex = 0;
-            let inHeader = (rowIndex < 2);
+            let inHeader = (rowIndex < headerRows);
             if (/Redistricted from the/.test($row.find('th,td').eq(0).text())) {
                 return; // skip row (Pennsylvania weirdness)
             }
@@ -41,7 +49,7 @@ function processTable($, $table) {
                     let text = $(cell).text().trim();
                     while (colspan--) {
                         if (inHeader) {
-                            heads[columnIndex++] = text.replace(/\s+/g, ' ');
+                            heads[columnIndex++] = text.replace(/\s+/g, ' ').replace(/\s*\[[^\]]+\]/g, '');
                         }
                         else {
                             rowData[heads[columnIndex++]] = text;
@@ -50,21 +58,25 @@ function processTable($, $table) {
                 }
             );
             if (!inHeader) {
-                let m = rowData.Location.match(/^([\w ]+)\s+(\d\d?|at-large)$/);
-                tableData.push({
-                    district: normalizeState(m[1]) + '-' + (m[2] === 'at-large' ? 1 : m[2]),
-                    pvi: rowData['2017 PVI'],
-                    party: rowData.Party,
-                    candidates: rowData.Candidates.split('\n').map(
-                        text => {
-                            let m = text.match(/^(.+?) \(([^)]+)\)/);
-                            return {
-                                name: m[1],
-                                party: m[2],
-                            }
-                        }),
-                });
-                console.log(tableData[tableData.length - 1]);
+                if (rowData.Location) {
+                    let m = rowData.Location.match(/^([\w ]+)\s+(\d\d?|at-large)$/);
+                    tableData.push({
+                        district: normalizeState(m[1]) + '-' + (m[2] === 'at-large' ? 1 : m[2]),
+                        pvi: rowData['2017 PVI'],
+                        party: rowData.Party,
+                        candidates: rowData.Candidates.split('\n').map(
+                            text => {
+                                let m = text.match(/^(.+?) \(([^)]+)\)/);
+                                return {
+                                    name: m[1],
+                                    party: m[2],
+                                }
+                            }),
+                    });
+                }
+                else {
+                    tableData.push(rowData);
+                }
             }
         }
     );
