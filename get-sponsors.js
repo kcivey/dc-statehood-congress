f6e1ef026d5676f475e0c6ff56d31cd5385af4ab
Keith C. Ivey <keith@iveys.org>
2018-09-11 22:20:07 -0400

    simplify the promise flow in get-sponsors.js

--- get-sponsors.js
+++ get-sponsors.js
@@ -5,58 +5,58 @@ const yaml = require('js-yaml');
 const _ = require('underscore');
 const ppc = require('propublica-congress').create(process.env.PROPUBLICA_API_KEY);
 
-getMembers().then(
-    members => {
-        const promises = ['hr1291', 's1278'].map(
-            billId => ppc.getAdditionalBillDetails(billId, 'cosponsors')
-        );
-        Promise.all(promises).then(
-            responses => {
-                console.log(yaml.safeDump(responses.map(
-                    r => {
-                        const results = r.results[0];
-                        let sponsors = results.cosponsors.map(
-                            c => ({
-                                id: c.cosponsor_id,
-                                name: c.name,
-                                title: c.cosponsor_title,
-                                state: c.cosponsor_state,
-                                party: c.cosponsor_party,
-                                uri: c.cosponsor_uri,
-                                date: c.date,
-                            })
-                        );
-                        sponsors.unshift(
-                            {
-                                id: results.sponsor_id,
-                                name: results.sponsor_name,
-                                title: results.sponsor_title,
-                                state: results.sponsor_state,
-                                party: results.sponsor_party,
-                                uri: results.sponsor_uri,
-                                date: results.introduced_date,
-                            }
-                        );
+Promise.all([
+    getMembers(),
+    ppc.getAdditionalBillDetails('hr1291', 'cosponsors'),
+    ppc.getAdditionalBillDetails('s1278', 'cosponsors'),
+])
+    .then(
+        responses => {
+            let members = responses.shift();
+            console.log(yaml.safeDump(responses.map(
+                r => {
+                    const results = r.results[0];
+                    let sponsors = results.cosponsors.map(
+                        c => ({
+                            id: c.cosponsor_id,
+                            name: c.name,
+                            title: c.cosponsor_title,
+                            state: c.cosponsor_state,
+                            party: c.cosponsor_party,
+                            uri: c.cosponsor_uri,
+                            date: c.date,
+                        })
+                    );
+                    sponsors.unshift(
+                        {
+                            id: results.sponsor_id,
+                            name: results.sponsor_name,
+                            title: results.sponsor_title,
+                            state: results.sponsor_state,
+                            party: results.sponsor_party,
+                            uri: results.sponsor_uri,
+                            date: results.introduced_date,
+                        }
+                    );
 
-                        return sponsors.map(
-                            sponsor => {
-                                const member = _.findWhere(members, {id: sponsor.id});
-                                if (!member) {
-                                    throw new Error(`id ${sponsor.id} not found`);
-                                }
-                                sponsor.inOffice = member.in_office;
-                                if (member.district) {
-                                    sponsor.district = member.district;
-                                }
-                                return sponsor;
+                    return sponsors.map(
+                        sponsor => {
+                            const member = _.findWhere(members, {id: sponsor.id});
+                            if (!member) {
+                                throw new Error(`id ${sponsor.id} not found`);
                             }
-                        );
-                    }
-                )))
-            }
-        );
-    }
-);
+                            sponsor.inOffice = member.in_office;
+                            if (member.district) {
+                                sponsor.district = member.district;
+                            }
+                            return sponsor;
+                        }
+                    );
+                }
+            )));
+            process.exit();
+        }
+    );
 
 function getMembers() {
     const promises = ['house', 'senate'].map(chamber => ppc.getMemberList(chamber));
