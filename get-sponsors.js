ef384c79292efa78bb2f130368014c7d2eda468d
Keith C. Ivey <keith@iveys.org>
2019-09-17 20:00:46 -0400

    rewrite get-sponsors.js to use async function and fix serious bug that was causing many senators to be omitted; change YAML structure and update

--- get-sponsors.js
+++ get-sponsors.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
+const assert = require('assert');
 const fs = require('fs');
 const yaml = require('js-yaml');
-const _ = require('underscore');
 const argv = require('yargs')
     .options({
         mongo: {
@@ -18,95 +18,67 @@ const currentCongress = 116;
 const ppc = require('propublica-congress').create(process.env.PROPUBLICA_API_KEY, currentCongress);
 const makeRaceCode = require('./lib/utils').makeRaceCode;
 
-getMembers()
-    .then(function (members) {
-        return ppc.getAdditionalBillDetails('s631', 'cosponsors')
-            .then(senateResponse => [members, senateResponse]);
-    })
-    .then(function (responses) {
-        return ppc.getAdditionalBillDetails('hr51', 'cosponsors')
-            .then(houseResponse => responses.concat([houseResponse]));
-    })
-    .then(
-        function (responses) {
-            const members = responses.shift();
-            let sponsors = [];
-            responses.forEach(
-                function (r) {
-                    const results = r.results[0];
-                    sponsors.push(
-                        {
-                            id: results.sponsor_id,
-                            name: results.sponsor_name,
-                            title: results.sponsor_title,
-                            state: results.sponsor_state,
-                            party: results.sponsor_party,
-                            uri: results.sponsor_uri,
-                            date: results.introduced_date,
-                        }
-                    );
-                    sponsors = sponsors.concat(
-                        results.cosponsors.map(
-                            function (c) {
-                                return {
-                                    id: c.cosponsor_id,
-                                    name: c.name,
-                                    title: c.cosponsor_title,
-                                    state: c.cosponsor_state,
-                                    party: c.cosponsor_party,
-                                    uri: c.cosponsor_uri,
-                                    date: c.date,
-                                };
-                            }
-                        )
-                    );
-                }
-            );
-            sponsors.forEach(
-                function (sponsor) {
-                    const member = _.findWhere(members, {id: sponsor.id});
-                    if (!member) {
-                        throw new Error(`id ${sponsor.id} not found`);
-                    }
-                    sponsor.inOffice = member.in_office;
-                    sponsor.code = makeRaceCode(member.state, member.district);
-                }
-            );
-            let promise = Promise.resolve();
-            if (db) {
-                const operations = sponsors.map(
-                    function (sponsor) {
-                        return {
-                            updateMany: {
-                                filter: {id: sponsor.id},
-                                update: {$set: sponsor},
-                                upsert: true,
-                            },
-                        };
-                    }
-                );
-                promise = promise.then(function () {
-                    db.createIndex('sponsors', {id: 1}, {unique: true})
-                        .then(() => db.bulkWrite('sponsors', operations))
-                        .then(() => db.close());
-                });
-            }
-            return promise.then(function () {
-                const codes = sponsors.map(s => s.code).sort();
-                const data = {};
-                codes.forEach(function (code) {
-                    data[code] = null;
-                });
-                sponsors.forEach(function (sponsor) {
-                    const s = Object.assign({}, sponsor); // clone
-                    data[s.code] = s;
-                    delete s.code;
-                });
-                return writeData(data);
-            });
+main().catch(console.trace);
+
+async function main() {
+    const members = await getMembers();
+    const senateResponse = await ppc.getAdditionalBillDetails('s631', 'cosponsors');
+    const houseResponse = await ppc.getAdditionalBillDetails('hr51', 'cosponsors');
+    const sponsors = [];
+    for (const r of [senateResponse, houseResponse]) {
+        const results = r.results[0];
+        sponsors.push( // main sponsor
+            {
+                code: null, // to set order
+                id: results.sponsor_id,
+                name: results.sponsor_name,
+                title: results.sponsor_title,
+                state: results.sponsor_state,
+                party: results.sponsor_party,
+                uri: results.sponsor_uri,
+                date: results.introduced_date,
+            },
+            ...results.cosponsors.map(function (c) {
+                return {
+                    code: null, // to set order
+                    id: c.cosponsor_id,
+                    name: c.name,
+                    title: c.cosponsor_title,
+                    state: c.cosponsor_state,
+                    party: c.cosponsor_party,
+                    uri: c.cosponsor_uri,
+                    date: c.date,
+                };
+            }),
+        );
+        for (const sponsor of sponsors) {
+            const member = members.find(m => m.id === sponsor.id);
+            assert(member, `id ${sponsor.id} not found`);
+            sponsor.inOffice = member.in_office;
+            sponsor.code = makeRaceCode(member.state, member.district);
         }
-    )
-    .catch(err => console.error(err));
+    }
+    if (db) {
+        const operations = sponsors.map(
+            function (sponsor) {
+                return {
+                    updateMany: {
+                        filter: {id: sponsor.id},
+                        update: {$set: sponsor},
+                        upsert: true,
+                    },
+                };
+            }
+        );
+        await db.createIndex('sponsors', {id: 1}, {unique: true});
+        await db.bulkWrite('sponsors', operations);
+        await db.close();
+    }
+    sponsors.sort(function (a, b) {
+        return a.code.localeCompare(b.code) || a.date.localeCompare(b.date) || a.name.localeCompare(b.name);
+    });
+    return writeData(sponsors);
+}
 
 function getMembers() {
     const promises = ['house', 'senate'].map(chamber => ppc.getMemberList(chamber));
